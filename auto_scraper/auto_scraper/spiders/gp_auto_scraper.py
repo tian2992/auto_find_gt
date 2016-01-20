2bbf220641041ea817efe346a889ce929e2ca53e
Sebastian Oliva <code@sebastianoliva.com>
2016-01-19 23:04:11 -0600

    extractfirsts

--- auto_scraper/auto_scraper/spiders/gp_auto_scraper.py
+++ auto_scraper/auto_scraper/spiders/gp_auto_scraper.py
@@ -37,7 +37,7 @@ class GpAutoScraperSpider(scrapy.Spider):
     def scrape_search_page(self, response):
         extracted_cars = response.xpath('//*[@id="formPag"]/div')
         for car in extracted_cars:
-            link = response.urljoin(car.xpath("a/@href").extract()[0])
+            link = response.urljoin(car.xpath("a/@href").extract_first())
             yield scrapy.Request(link, callback=self.parse_car_page)
             ### div.cdata:nth-child(1) > span:nth-child(2)
             ### model div.cdata:nth-child(3) > span:nth-child(2)
@@ -46,12 +46,12 @@ class GpAutoScraperSpider(scrapy.Spider):
 
     def parse_car_page(self, response):
         auto = AutoItem()
-        auto["gp_id"] = response.xpath('//*[@id="carroCabecera"]/h1').extract()
+        auto["gp_id"] = response.xpath('//*[@id="carroCabecera"]/h1').extract_first()
         #TODO: fix xpaths...
-        auto["brand"] = response.xpath('/html/body/div/section/div[1]/div[4]/div[1]/span/text()').extract()
-        auto["model"] = response.xpath('/html/body/div/section/div[1]/div[4]/div[3]/span/text()').extract()
-        auto["color"] = response.xpath('/html/body/div/section/div[1]/div[4]/div[4]/span/text()').extract()
-        auto["kms"] = response.xpath('/html/body/div/section/div[1]/div[4]/div[9]/span/text()').extract()
-        auto["ccs"] = response.xpath('/html/body/div/section/div[1]/div[4]/div[6]/span/text()').extract()
-        auto["year"] = response.xpath('/html/body/div/section/div[1]/div[4]/div[8]/span/text()').extract()
+        auto["brand"] = response.xpath('/html/body/div/section/div[1]/div[4]/div[1]/span/text()').extract_first()
+        auto["model"] = response.xpath('/html/body/div/section/div[1]/div[4]/div[3]/span/text()').extract_first()
+        auto["color"] = response.xpath('/html/body/div/section/div[1]/div[4]/div[4]/span/text()').extract_first()
+        auto["kms"] = response.xpath('/html/body/div/section/div[1]/div[4]/div[9]/span/text()').extract_first()
+        auto["ccs"] = response.xpath('/html/body/div/section/div[1]/div[4]/div[6]/span/text()').extract_first()
+        auto["year"] = response.xpath('/html/body/div/section/div[1]/div[4]/div[8]/span/text()').extract_first()
         return auto
